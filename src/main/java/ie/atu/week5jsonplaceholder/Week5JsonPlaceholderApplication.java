2071e8f1398f0fb7d2dab60d8a7d1de219584ef4
rakib <G00408644@atu.ie>
2023-10-24 15:27:20 +0100

    base

---
+++ src/main/java/ie/atu/week5jsonplaceholder/Week5JsonPlaceholderApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5jsonplaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5JsonPlaceholderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5JsonPlaceholderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week5jsonplaceholder/Week5JsonPlaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5jsonplaceholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5JsonPlaceholderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
